582dd0751b2fa0a5240c531a044349fd018abdb7
zy2ba <zy2bas@gmail.com>
2016-06-25 04:39:46 +0300

    Запуск с разбором параметров и русским языком

--- TranslatorToCNF/TranslatorToCNF/TranslatorToCNF.cpp
+++ TranslatorToCNF/TranslatorToCNF/TranslatorToCNF.cpp
@@ -3,9 +3,24 @@
 
 #include "stdafx.h"
 
+using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    return 0;
+	setlocale(LC_ALL, "Russian_Russia.1251");//обеспечивает русский язык
+	int argn = 1;
+	while (argn < argc) {
+		if ( (strcmp(argv[argn], "help") == 0) || (strcmp(argv[argn], "HELP") == 0)) {
+			cout << "Здесь когда-то будет help" << endl;
+			argn++;
+		}
+		else {
+			cout << "Обнаружена нераспознанная комманда \"" 
+				<< argv[argn] << "\", она была пропущена.\n Запустите программу с аргументом help для получения справки" << endl;
+			argn++;
+		}
+	}
+	getchar();
+	return 0;
 }
 
--- TranslatorToCNF/TranslatorToCNF/stdafx.h
+++ TranslatorToCNF/TranslatorToCNF/stdafx.h
@@ -9,6 +9,8 @@
 
 #include <stdio.h>
 #include <tchar.h>
+//Добавленно мной:
+#include <iostream>
 
 
 
